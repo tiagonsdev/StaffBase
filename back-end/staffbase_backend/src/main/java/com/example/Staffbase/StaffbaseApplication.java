dc7f68d02c6fc91456d7f627d35f30a904d69773
Tiago Novaes <tiagonsousadev@gmail.com>
2025-07-27 16:12:05 -0300

    projeto springboot criado

---
+++ back-end/staffbase_backend/src/main/java/com/example/Staffbase/StaffbaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.Staffbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffbaseApplication.class, args);
+	}
+
+}
